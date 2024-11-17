bf0b467805d58d84d73317c7850195cc4064cc56
LixiosDelios <alexandre.guillaud@epitech.eu>
2024-11-17 11:44:52 +0100

    fix : wrong index modulo

--- src/Neurone/neuron_link.cpp
+++ src/Neurone/neuron_link.cpp
@@ -10,11 +10,11 @@ Neuron_link::Neuron_link(const Gene &gen)
     this->in_neuron = gen[0] ? INPUT_NEURON : LAYER_NEURON;
     for (int i = 1; i < INDEX_SIZE; ++i)
         res = (res << 1) + gen[i];
-    this->in_index = res % INPUT_SIZE;
+    this->in_index = res % (gen[0] ? INPUT_SIZE : LAYER_NEURON_LENGHT);
     this->out_neuron = gen[INDEX_SIZE] ? LAYER_NEURON : OUTPUT_NEURON;
     for (int i = INDEX_SIZE; i < 2 * INDEX_SIZE; ++i)
         res = (res << 1) + gen[i];
-    this->out_index = res % OUTPUT_SIZE;
+    this->out_index = res % (gen[INDEX_SIZE] ? LAYER_NEURON_LENGHT : OUTPUT_SIZE);
     for (int i = 2 * INDEX_SIZE; i < GEN_LENGHT; ++i)
         res = (res << 1) + gen[i];
     this->weight = (res / powf(2, GEN_LENGHT - 2)) - DEFAULT_INPUT_SIZE;
